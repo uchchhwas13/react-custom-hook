8794bf04617da0b06fb61ca53c9316c06f9c5f3a
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-22 11:25:05 +0600

    Moving common code into a react hook useOnlineStatus

--- React-hook/src/App.jsx
+++ React-hook/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import StatusBar from './StatusBar.jsx'
+import SaveButton  from './SaveButton.jsx'
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <StatusBar />
+      <SaveButton />
     </>
   )
 }
